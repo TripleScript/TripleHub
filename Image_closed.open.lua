fb015d3d40cbb37fbf923262168960bcf489f987
TripleScript <secondtimelast28@gmail.com>
2024-08-10 19:43:38 +0800

    Update Image_closed.open.lua

--- Image_closed.open.lua
+++ Image_closed.open.lua
@@ -1,35 +1,27 @@
 local image = {}
 
--- Gui to Lua
--- Version: 3.2
-
--- Instances:
-
 local ScreenGui = Instance.new("ScreenGui")
-local ImageButton = Instance.new("ImageButton")
-local UICorner = Instance.new("UICorner")
-
---Properties:
-
+ScreenGui.Name = "ScreenGui"
 ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-ImageButton.Parent = ScreenGui
-ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
-ImageButton.BorderSizePixel = 0
-ImageButton.Position = UDim2.new(0.480349272, 0, -0.000868847885, 0)
-ImageButton.Size = UDim2.new(0, 46, 0, 48)
-ImageButton.Image = "rbxassetid://17547308586"
-
-UICorner.Parent = ImageButton
-
 ScreenGui.ResetOnSpawn = false
-ImageButton.Active = true
-ImageButton.Draggable = true
-ImageButton.MouseButton1Down:Connect(function()
-        Library:ToggleUI()
+ 
+local Toggle = Instance.new("TextButton")
+Toggle.Name = "Toggle"
+Toggle.Parent = ScreenGui
+Toggle.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Toggle.Position = UDim2.new(0, 0, 0.454706937, 0)
+Toggle.Size = UDim2.new(0, 90, 0, 38)
+Toggle.Font = Enum.Font.SourceSans
+Toggle.Text = "Triple Hub"
+Toggle.TextColor3 = Color3.fromRGB(248, 248, 248)
+Toggle.TextSize = 28.000
+Toggle.Draggable = true
+Toggle.MouseButton1Click:connect(function()
+    Library:ToggleUI()
 end)
+ 
+local Corner = Instance.new("UICorner")
+Corner.Name = "Corner"
+Corner.Parent = Toggle
 
 return image
-
